d38fcfdfa12fca99f00f1310715d5d96f31732b8
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-02-25 01:16:20 +0200

    Added DDNS abstract contract

---
+++ contracts/DomainNameSystem.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.17;
+
+import "./common/Killable.sol";
+import "./libraries/SafeMath.sol";
+import "./abstracts/DomainNameSystemBase.sol";
+
+contract DomainNameSystem is Killable, DomainNameSystemBase {
+
+}
