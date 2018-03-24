086d6dbe8e37f4acc7d648d2e9ea21e0c835d483
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-24 21:17:40 +0200

    Added basic html for the transfering of domain form

--- src/components/domains/TransferDomainForm.js
+++ src/components/domains/TransferDomainForm.js
@@ -1,7 +1,34 @@
 import React, {PropTypes} from 'react';
+import TextInput from './../common/TextInput';
 
-const TransferDomainForm = () => {
+const TransferDomainForm = ({domainName, transferToAddress, onSubmit, errors, onChange}) => {
+    return (
+        <div>
+            <TextInput
+                name="domainName"
+                onChange={onChange}
+                label="Domain"
+                error={errors.domainName}
+                value={domainName}
+            />
+            <TextInput
+                name="tranferToAddress"
+                onChange={onChange}
+                label="Transfer to address"
+                error={errors.transferToAddress}
+            />
 
+            <input type="button" onClick={onSubmit} value="Transfer" className="btn btn-primary"/>
+        </div>
+    );
+};
+
+TransferDomainForm.propTypes = {
+    domainName: PropTypes.string.isRequired,
+    transferToAddress: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
 export default TransferDomainForm;
